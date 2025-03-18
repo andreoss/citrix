bede74f49a21edadd500d1d3f617eb6e26203f35
andreoss <andreoss@sdf.org>
2025-03-17 23:03:23 -0400

    add xdotool

--- flake.nix
+++ flake.nix
@@ -4,27 +4,45 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   description = "Citrix Client";
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         systems = lib.systems.flakeExposed;
         lib = nixpkgs.lib;
         eachSystem = lib.genAttrs systems;
-      in {
+      in
+      {
         legacyPackages = import nixpkgs {
           inherit system;
-          config = { allowUnfree = true; };
+          config = {
+            allowUnfree = true;
+          };
         };
-        packages.wfica = let pkgs = self.legacyPackages.${system};
-        in pkgs.writeShellApplication {
-          name = "wfica";
-          checkPhase = ":";
-          runtimeInputs = [ pkgs.xorg.xauth ];
-          text = builtins.readFile (pkgs.substituteAll {
-            src = ./jailed-wfica;
-            citrix = pkgs.citrix_workspace;
-          });
-        };
+        packages.wfica =
+          let
+            pkgs = self.legacyPackages.${system};
+          in
+          pkgs.writeShellApplication {
+            name = "wfica";
+            checkPhase = ":";
+            runtimeInputs = [
+              pkgs.xorg.xauth
+              pkgs.xdotool
+            ];
+            text = builtins.readFile (
+              pkgs.substituteAll {
+                src = ./jailed-wfica;
+                citrix = pkgs.citrix_workspace;
+              }
+            );
+          };
         defaultPackage = self.packages.${system}.wfica;
-      });
+      }
+    );
 }
